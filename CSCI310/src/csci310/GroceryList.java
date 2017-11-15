affd2a745d03f0fd77d65a6b05e5ae7fc97aaa36
djneely <djneely@mavs.coloradomesa.edu>
2017-11-14 21:13:17 -0700

    add override compareTo method

--- CSCI310/src/csci310/GroceryList.java
+++ CSCI310/src/csci310/GroceryList.java
@@ -9,6 +9,30 @@ package csci310;
  *
  * @author Dalton
  */
-public class GroceryList {
+public class GroceryList implements Groceries {
+
+    @Override
+    public int compareTo(Directive to) {
+        if(to instanceof RelayDirective){
+            RelayDirective relay = (RelayDirective) to;
+            if(gpio < relay.gpio) return  -1;
+            if(gpio > relay.gpio) return 1;
+            if(setting == false && relay.setting == true) return -1;
+            if(setting == true && relay.setting == false) return 1;
+            return 0;
+        }else{
+            return this.getClass().getName().compareTo(to.getClass().getName());
+        }
+    }
+    
+    @Override
+    public int compareTo(Groceries o) {
+        if(o instanceof Groceries){
+            Groceries groceries = (Groceries) o;
+            return 0;
+        }else{
+            return this.getClass().getName().compareTo(o.getClass().getName());
+        }
+    }
     
 }
